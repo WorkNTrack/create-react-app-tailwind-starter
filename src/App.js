2c93ce567f8316ca19a6c024dba7975f4cf47313
Akhil Gautam <akhilgautam123@gmail.com>
2020-11-19 23:30:16 +0530

    Configured Tailwind-React with PurgeCss for Production builds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full w-full flex justify-center items-center">
+      <a href="https://github.com/WorkNTrack/tailwind_ui" className="px-8 py-4 bg-gradient-to-r from-gray-100 to-gray-200 rounded-md shadow-2xl font-semibold text-xl">
+        TAILWIND<span className="font-extralight text-sm"> (click here for more from workNTrack)</span>
+      </a>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  purge: ['./src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
