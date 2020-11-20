8481cf50fc1911aa9e2021874680edd44be86e49
Akhil Gautam <akhilgautam123@gmail.com>
2020-11-20 17:53:21 +0530

    Added apply usecase as an example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="h-full w-full flex justify-center items-center">
-      <a href="https://github.com/WorkNTrack/tailwind_ui" className="px-8 py-4 bg-gradient-to-r from-gray-100 to-gray-200 rounded-md shadow-2xl font-semibold text-xl">
-        TAILWIND<span className="font-extralight text-sm"> (click here for more from workNTrack)</span>
+      <a
+        href="https://github.com/WorkNTrack/tailwind_ui"
+        className="animated-card"
+      >
+        TAILWIND
+        <span className="font-extralight text-sm">
+          {' '}
+          (click here for more from workNTrack)
+        </span>
       </a>
     </div>
   );
